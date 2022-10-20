b6bdfe1cf215e4cb2415d05c6582d3c7821fdbad
Srini Marreddy <srinivasa.marreddy@resideo.com>
2022-10-19 21:21:45 -0500

    - include mapped error message in toString

--- lib/flutter_ble_lib.dart
+++ lib/flutter_ble_lib.dart
@@ -41,18 +41,13 @@ import 'package:flutter_ble_lib/src/_internal.dart';
 import 'package:flutter_ble_lib/src/_managers_for_classes.dart';
 import 'package:flutter_ble_lib/src/util/_transaction_id_generator.dart';
 
+import 'error/ble_error_codes_to_message_mapping.dart';
 import 'src/_managers_for_classes.dart';
 
-part 'error/ble_error.dart';
-
 part 'ble_manager.dart';
-
 part 'characteristic.dart';
-
 part 'descriptor.dart';
-
+part 'error/ble_error.dart';
 part 'peripheral.dart';
-
 part 'scan_result.dart';
-
 part 'service.dart';
